611e5413aec8397df1af82d56f20bd01088595b3
vladrus13 <vladrus13rus@yandex.ru>
2021-03-29 14:09:53 +0300

    Init commit

---
+++ src/main/java/ru/vladrus13/graphvisualizer/Launcher.java
@@ -0,0 +1,12 @@
+package ru.vladrus13.graphvisualizer;
+
+/**
+ * Launcher class
+ * @author vladrus13 on 29.03.2021
+ **/
+public class Launcher {
+
+    public static void main(String[] args) {
+
+    }
+}
